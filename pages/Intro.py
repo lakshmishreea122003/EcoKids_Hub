c3e1bb27227845bd528585b72aef4e4faef8baff
Lakshmi Shree A <lakshmishreea122003@gmail.com>
2023-07-23 08:23:31 -0500

    Add project

---
+++ pages/Intro.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Into", page_icon="🌱")
+
+st.markdown(" <h1 style='color: green; font-style: italic; font-family: Comic Sans MS; '>I am Lakshmi, and this is my project EcoKids Hub for Hacks for Hackers hackathon hosted by MLH</h1>", unsafe_allow_html=True)
+
